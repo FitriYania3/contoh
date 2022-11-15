f6252fed8642d92b29b8ca9246f187aa2b3ac7ee
FitriYania3 <fitriiyani98@gmail.com>
2022-11-15 12:24:59 +0700

    fitri

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array {
+
+    public static void main(String[] args) {
+        String [] hobby = {"futsal", "vollly", "basket", "renang", "batminton"};
+        System.out.println(hobby[1]);
+        
+        System.out.print("hobby");
+        System.out.println(hobby[2]);
+
+        System.out.print("hobby");
+        System.out.println(hobby[3]);
+    }
+}
